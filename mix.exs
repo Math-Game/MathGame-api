36a4c473576c8be3b0b74d486b858ec02de78d07
Niek125 <niek.sleddens@gmail.com>
2020-06-24 23:46:13 +0200

    cors

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Api.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
       {:absinthe, "~> 1.5"},
-      {:absinthe_plug, "~> 1.5"}
+      {:absinthe_plug, "~> 1.5"},
+      {:cors_plug, "~> 1.5"},
     ]
   end
 
