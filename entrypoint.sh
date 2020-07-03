9f1bf89a380ab247543cfeaf21819722c3a35981
Niek125 <niek.sleddens@gmail.com>
2020-07-03 16:56:44 +0200

    echo host

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,9 @@
 # Wait until Postgres is ready
 while ! pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER
 do
+  ping -c 3 $DB_HOST
+  echo $DB_HOST
+  echo $DB_USER
   echo "$(date) - waiting for database to start"
   sleep 2
 done
