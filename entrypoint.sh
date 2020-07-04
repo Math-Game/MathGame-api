5ce93f7f62e2ad37cb62184c4ae25a89aeb121c0
Niek125 <niek.sleddens@gmail.com>
2020-07-04 19:28:54 +0200

    same as while

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@
 res=!$(pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER)
 echo "res is: $res"
 
-while ! $res; do
-  res=$(pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER)
+while $res; do
+  res=!$(pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER)
   echo "res is: $res"
   echo "$(date) - waiting for database to start"
   sleep 2
