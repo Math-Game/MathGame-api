3aeb80d3483a8300ec4fb116b9fbcb940b48adc2
Niek125 <niek.sleddens@gmail.com>
2020-07-04 19:17:01 +0200

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,15 @@
 
 # Wait until Postgres is ready
 res=$(pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER)
-echo $res
-while ! $res
-do
+echo "res is: $res"
+
+while ! $res; do
   res=$(pg_isready -q -h $DB_HOST -p 5432 -U $DB_USER)
-  echo $res
+  echo "res is: $res"
   echo "$(date) - waiting for database to start"
   sleep 2
 done
 
-./prod/rel/demo/bin/demo eval Demo.Release.migrate
+./prod/rel/api/bin/api eval Api.Release.migrate
 
-./prod/rel/demo/bin/demo start
+./prod/rel/api/bin/api start
