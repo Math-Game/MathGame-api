0dafdfea9fe67e72d9854b6d69dff9fcd85bc921
Niek125 <niek.sleddens@gmail.com>
2020-07-05 21:41:25 +0200

    test to see if user matters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 # Docker entrypoint script.
-pg_isready -p 5432 -h $DB_HOST -q; echo $?
+pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q; echo $?
 # Wait until Postgres is ready
-res="$(pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q; echo $?)"
+res="$(pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q)"
 echo "res is: ${res}"
 
 while $res
 do
-  res="$(pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q; $?)"
+  res="$(pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q)"
   echo "res is: ${res}"
   echo "$(date) - waiting for database to start"
   sleep 2
