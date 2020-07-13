f6f861c57d6c44a4784a075eb196f7d1a935de7f
Niek125 <niek.sleddens@gmail.com>
2020-07-13 16:50:33 +0200

    pg_isready is back

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,16 @@
-#!/bin/sh
-# Docker entrypoint script.
-# Wait until Postgres is ready
+!/bin/sh
+Docker entrypoint script.
+Wait until Postgres is ready
 
-# TODO fix pg_isready
 
-#while ! pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q; do
-#  pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q
-#  echo $?
-#  echo "$(date) - waiting for database to start"
-#  sleep 2
-#done
+while ! pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q; do 
+ ping $DB_HOST
+ pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q
+ echo $?
+ echo "$(date) - waiting for database to start"
+ sleep 2
+done
 
-sleep 10
 
 ./prod/rel/api/bin/api eval Api.Release.migrate
 
