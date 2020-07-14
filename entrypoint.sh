ff74c6721958cf8cc13ef32aed9322e4b954f603
Niek125 <niek.sleddens@gmail.com>
2020-07-14 15:38:18 +0200

    Ping format

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ Wait until Postgres is ready
 
 
 while ! pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q; do 
- ping -c 6 "db.math.game"
+ ping -c 3 db.math.game
  pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q
  echo $?
  echo "$(date) - waiting for database to start"
