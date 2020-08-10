874765569ca02ca1e6a1518ddc68cea534d7e2db
Niek125 <niek.sleddens@gmail.com>
2020-08-10 12:14:10 +0200

    first line fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
-# # !/bin/sh
-# # Docker entrypoint script.
-# # Wait until Postgres is ready
+#!/bin/sh
+# Docker entrypoint script.
+# Wait until Postgres is ready
 
-# while ! pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q; do
-#     ping -c 3 db.math.game
-#     pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q
-#     echo $?
-#     echo "$(date) - waiting for database to start"
-#     sleep 2
-# done
+while ! pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q; do
+    ping -c 3 db.math.game
+    pg_isready -p 5432 -h $DB_HOST -U $DB_USER -q
+    echo $?
+    echo "$(date) - waiting for database to start"
+    sleep 2
+done
 
-# ./prod/rel/api/bin/api eval Api.Release.migrate
+./prod/rel/api/bin/api eval Api.Release.migrate
 
-# ./prod/rel/api/bin/api start
+./prod/rel/api/bin/api start
